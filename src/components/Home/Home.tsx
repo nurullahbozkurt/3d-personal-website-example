91bd7b8e54032b48ca06c013b35f14a15a4b9e50
nurullahbozkurt <nurullahbozkurtt@gmail.com>
2023-03-11 15:50:35 +0300

    add three layout

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Layout } from "@/components/Layout";
+import { Three } from "@/components/Three";
+
+type Props = {};
+
+const Home = (props: Props) => {
+  return (
+    <Layout>
+      <Three />
+    </Layout>
+  );
+};
+
+export default Home;
